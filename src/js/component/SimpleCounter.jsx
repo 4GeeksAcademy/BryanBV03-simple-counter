df691a28e2d7b911ec759ea1fe5ecd1c0c182241
Bryan <168112800+BryanBV03@users.noreply.github.com>
2024-07-19 20:26:51 +0000

    simple-counter

---
+++ src/js/component/SimpleCounter.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+
+function SimpleCounter(props) {
+    const [contador, setContador] = useState(props.contador || 0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setContador(prevContador => prevContador +1);
+        }, 1000);
+
+        // Limpiar el intervalo cuando el componente se desmonte
+        return () => clearInterval(intervalId);
+    }, []);
+
+
+
+    return (<div className="bigCounter">
+        <div className="calendar">
+            <i className="far fa-clock"></i>
+        </div>
+        <div className="four">{Math.floor(contador / 1000) % 10}</div>
+        <div className="three">{ Math.floor(contador / 100) % 10}</div>
+        <div className="two">{Math.floor(contador / 10) % 10}</div>
+        <div className="one">{ Math.floor(contador / 1) % 10}</div>
+    </div>);
+}
+
+export default SimpleCounter
+ 
